2bbf98832ad48533d94eb12d634396df45a97f82
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-05-11 22:43:28 +0530

    fixed - admin is not correct_user issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :correct_user]
   before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :admin_user, only: [:edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
