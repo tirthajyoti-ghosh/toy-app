3e4975117d095781e610341506f01d10e781ddbc
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-05-23 12:58:49 +0530

    added tests for user model validations  =>        presence, uniqueness, format of name and email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,19 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, 
+                   length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, 
+            length: { maximum: 255 }, 
+            format: { with: VALID_EMAIL_REGEX }, 
+            uniqueness: true
 
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  # has_secure_password
+  # validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
